5113956cb1b958909230e745ab6f5d192b948ef2
kmizuk <mizukake213@gmail.com>
2020-06-21 17:44:15 +0900

    scaffolding

--- src/components/App.js
+++ src/components/App.js
@@ -1,24 +1,24 @@
-import React from 'react';
+import React from "react";
+import { BrowserRouter, Route, Link } from "react-router-dom";
 
+const PageOne = () => {
+  return <div>PageOne</div>;
+};
+
+const PageTwo = () => {
+  return <div>PageTwo</div>;
+};
 
 const App = () => {
-    return ( <div>
-        <div className="ui labeled button" tabindex="0">
-    <div className="ui red button">
-      <i className="heart icon"></i> Like
+  return (
+    <div>
+      <BrowserRouter>
+        <div>
+          <Route path="/" exact component={PageOne} />
+          <Route path="/pagetwo" component={PageTwo} />
+        </div>
+      </BrowserRouter>
     </div>
-    <a className="ui basic red left pointing label">
-      1,048
-    </a>
-  </div>
-  <div className="ui labeled button" tabindex="0">
-    <div className="ui basic blue button">
-      <i className="fork icon"></i> Forks
-    </div>
-    <a className="ui basic left pointing blue label">
-      1,048
-    </a>
-  </div>
-  </div> );
-}
+  );
+};
 export default App;
